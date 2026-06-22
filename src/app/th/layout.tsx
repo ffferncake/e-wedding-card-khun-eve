a677a46ff89de98e60e7eb570312822509b3a1cc
ffferncake <fernnichanun@gmail.com>
2026-06-22 22:43:00 +0900

    Fix 🐛: change og image (horizontal)

--- src/app/th/layout.tsx
+++ src/app/th/layout.tsx
@@ -13,7 +13,7 @@ export const metadata = {
     siteName: "EVE&NUN Wedding Invitation",
     images: [
       {
-        url: "https://eve-nun.vercel.app/images/bg_updated_1 (1) (2).jpg",
+        url: "https://eve-nun.vercel.app/images/og_image.jpg",
         width: 800,
         height: 600,
         alt: "EVE&NUN Wedding Photo",
@@ -36,7 +36,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         />
         <meta
           property="og:image"
-          content="https://eve-nun.vercel.app/images/bg_updated_1 (1) (2).jpg"
+          content="https://eve-nun.vercel.app/images/og_image.jpg"
         />
         <meta property="og:url" content="https://eve-nun.vercel.app/" />
         <meta property="og:type" content="website" />
